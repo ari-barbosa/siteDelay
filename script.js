eee795f8ee57a5fa264b092b7af85fb0e4b5fefb
ari-barbosa <abarbosa.ipms@gmail.com>
2022-12-08 09:42:00 -0300

    versão02 projeto Delay

---
+++ script.js
@@ -0,0 +1,15 @@
+function paraCima() {
+const botao = document.querySelector("#back-to-top");
+
+botao.addEventListener('click', function() {
+ window.scrollTo(0, 0);
+});
+}
+
+function paraBaixo() {      
+       window.scrollTo(3000, 3000);
+      }
+
+
+
+/// mudar imagem
